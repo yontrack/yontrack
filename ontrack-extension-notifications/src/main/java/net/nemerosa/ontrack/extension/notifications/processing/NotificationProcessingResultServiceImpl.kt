6079a46922e8d2577a3e5c3c85d1fb2ae319be51
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2026-05-28 08:18:43 +0200

    #1603 Refactor NotificationProcessingResultServiceImpl to remove code duplication

--- ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/processing/NotificationProcessingResultServiceImpl.kt
+++ ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/processing/NotificationProcessingResultServiceImpl.kt
@@ -15,20 +15,14 @@ class NotificationProcessingResultServiceImpl(
 
     override fun getActualizedResult(processingResult: NotificationProcessingResult<*>): NotificationProcessingResult<*>? {
         val recordId = processingResult.recordId
-        val record = securityService.asAdmin { notificationRecordingService.findRecordById(recordId) } ?: return null
-        val channel = notificationChannelRegistry.findChannel(record.channel) ?: return null
-        val result = channel.getNotificationResult(record) ?: return null
-        return NotificationProcessingResult(
-            recordId = recordId,
-            result = result,
-        )
+        val result = getActualizedResult(recordId) ?: return null
+        return NotificationProcessingResult(recordId = recordId, result = result)
     }
 
     override fun getActualizedResult(recordId: String): NotificationResult<*>? {
         val record = securityService.asAdmin { notificationRecordingService.findRecordById(recordId) } ?: return null
         val channel = notificationChannelRegistry.findChannel(record.channel) ?: return null
-        val result = channel.getNotificationResult(record) ?: return null
-        return result
+        return channel.getNotificationResult(record)
     }
 
 }
